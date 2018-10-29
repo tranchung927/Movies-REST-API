0d2bd6c78c38090fea73b39906f3192184f2a5cc
tranchung927 <tranchung927@gmail.com>
2018-10-29 21:13:23 +0700

    update read data

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let express = require("express")
+let express = require('express')
 let app = express()
 
+const datastore = require('./datastore.json')
+
+let handlerData = (req, res) => {
+    return res.send(datastore)
+}
+
 app.get('/', (req, res) => {
-    res.send("Hello World!!")
+    return res.send("Hello World!!")
 })
+
+app.get('/data', handlerData)
 
 app.listen(8000, () => {
     console.log("Server started at 127.0.0.1:8000")
